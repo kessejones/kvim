b46d8e7c454ae69ba248fe54a1722c9045222e63
Kesse Jones <kjonesfc@outlook.com>
2021-10-12 13:57:36 -0300

    refactor init colorscheme

--- lua/kvim/colors/init.lua
+++ lua/kvim/colors/init.lua
@@ -1,14 +1,11 @@
 local M = {  }
 local settings = require('kvim.config.settings')
 
-local cmd = vim.cmd
-local has = vim.fn.has
-
 function M:init()
     vim.g.tokyonight_style = "night"
     vim.g.tokyonight_enable_italic = true
 
-    if not has('mac') then
+    if not vim.fn.has('mac') then
         settings:set("termguicolors", true)
     end
 
