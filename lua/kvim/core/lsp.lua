742895d83d85a01e25e6011fffd2f47f6fb40a0c
Kesse Jones <kjonesfc@oulook.com>
2021-12-02 14:58:58 -0300

    added mappings to lsp action

--- lua/kvim/core/lsp.lua
+++ lua/kvim/core/lsp.lua
@@ -4,7 +4,16 @@ local keymapping = require('kvim.keymappings')
 function M:init() 
     local mapping = {
         normal_mode = {
-            ["K"] = "<cmd>:LspHover<CR>"
+            ["K"] = "<cmd>:LspHover<CR>",
+            ["gd"] = "<cmd>:LspDefinition<CR>",
+            ["gs"] = "<cmd>:LspDocumentSymbolSearch<CR>",
+            ["gS"] = "<cmd>:LspWorkspaceSymbolSearch<CR>",
+            ["gr"] = "<cmd>:LspReferences<CR>",
+            ["gi"] = "<cmd>:LspImplementation<CR>",
+            ["gt"] = "<cmd>:LspTypeDefinition<CR>",
+            ["<Leader>rn"] = "<cmd>:LspRename<CR>",
+            ["[g"] = "<cmd>:LspPreviousDiagnostic<CR>",
+            ["]g"] = "<cmd>:LspNextDiagnostic<CR>",
         }
     }
     keymapping:load(mapping)
