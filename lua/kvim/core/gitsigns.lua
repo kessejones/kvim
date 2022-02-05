2c71ce9b3c96286fdd62cd337f0e43c3f2d3a741
Kesse Jones <kjonesfc@outlook.com>
2022-02-04 21:49:38 -0300

    (feat) added key binding to gitsigns toggle deleted lines

--- lua/kvim/core/gitsigns.lua
+++ lua/kvim/core/gitsigns.lua
@@ -1,8 +1,16 @@
+local keymappings = require("kvim.keymappings")
+
 local M = {}
 
 function M:init()
     require("gitsigns").setup({
         current_line_blame = false,
+    })
+
+    keymappings:load({
+        normal_mode = {
+            ["<Leader>td"] = ":Gitsigns toggle_deleted<CR>",
+        },
     })
 end
 
