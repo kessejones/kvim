d5de787a44f6f6e8207b58bea9c40c88cd636137
Kesse Jones <kjonesfc@outlook.com>
2022-02-07 13:25:39 -0300

    (feat) added signs to diagnostics and custom handlers

--- lua/kvim/core/lsp.lua
+++ lua/kvim/core/lsp.lua
@@ -37,8 +37,10 @@ local servers = {
     elixirls = {
         cmd = { vim.fn.expand("$HOME") .. "/.local/bin/elixir-ls" },
     },
+    clangd = {},
 }
-function M:init()
+
+function M:lsp_config()
     local on_attach = function(client)
         client.resolved_capabilities.document_formatting = false
 
@@ -49,9 +51,9 @@ function M:init()
                 ["gr"] = "<cmd>:lua vim.lsp.buf.references()<CR>",
                 ["gi"] = "<cmd>:lua vim.lsp.buf.implementation()<CR>",
                 ["<Leader>rn"] = "<cmd>:lua vim.lsp.buf.rename()<CR>",
-                ["g["] = "<cmd>:lua vim.lsp.diagnostic.goto_prev()<CR>",
-                ["g]"] = "<cmd>:lua vim.lsp.diagnostic.goto_next()<CR>",
-                ["<S-u>"] = "<cmd>:lua vim.lsp.diagnostic.show_line_diagnostics()<CR>",
+                ["g["] = "<cmd>:lua vim.diagnostic.goto_prev()<CR>",
+                ["g]"] = "<cmd>:lua vim.diagnostic.goto_next()<CR>",
+                ["<S-u>"] = "<cmd>:lua vim.diagnostic.show_line_diagnostics()<CR>",
                 ["<Leader>ff"] = "<cmd>:lua vim.lsp.buf.formatting()<CR>",
             },
             visual_mode = {
@@ -69,6 +71,52 @@ function M:init()
 
         lspconfig[server].setup(config)
     end
+end
+
+function M:handlers()
+    local signs = {
+        { name = "DiagnosticSignError", text = "" },
+        { name = "DiagnosticSignWarn", text = "" },
+        { name = "DiagnosticSignHint", text = "" },
+        { name = "DiagnosticSignInfo", text = "" },
+    }
+
+    for _, sign in ipairs(signs) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    end
+
+    local config = {
+        virtual_text = true,
+        signs = {
+            active = signs,
+        },
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = false,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+        },
+    }
+
+    vim.diagnostic.config(config)
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded",
+    })
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "rounded",
+    })
+end
+
+function M:init()
+    self:lsp_config()
+    self:handlers()
 end
 
 return M
