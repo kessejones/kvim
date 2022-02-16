c119f8571260e525b102a0debb92a4b154949094
Kesse Jones <kjonesfc@outlook.com>
2022-02-16 15:51:55 -0300

    (refactor) reuse module path to load core plugins

--- lua/kvim/core/init.lua
+++ lua/kvim/core/init.lua
@@ -1,29 +1,30 @@
 local M = {}
 
 local modules = {
-    "kvim.core.cmp",
-    "kvim.core.commentary",
-    "kvim.core.git-blame-line",
-    "kvim.core.git-fugitive",
-    "kvim.core.gitsigns",
-    "kvim.core.indent-blankline",
-    "kvim.core.lsp",
-    "kvim.core.lualine",
-    "kvim.core.maximizer",
-    "kvim.core.mix-fugitive",
-    "kvim.core.null-ls",
-    "kvim.core.nvimtree",
-    "kvim.core.project",
-    "kvim.core.startify",
-    "kvim.core.telescope",
-    "kvim.core.toggleterm",
-    "kvim.core.treesitter",
-    "kvim.core.bufferline",
+    "cmp",
+    "commentary",
+    "git-blame-line",
+    "git-fugitive",
+    "gitsigns",
+    "indent-blankline",
+    "lsp",
+    "lualine",
+    "maximizer",
+    "mix-fugitive",
+    "null-ls",
+    "nvimtree",
+    "project",
+    "startify",
+    "telescope",
+    "toggleterm",
+    "treesitter",
+    "bufferline",
 }
 
 function M.init()
+    local module_base = "kvim.core."
     for _, path_module in ipairs(modules) do
-        local module = require(path_module)
+        local module = require(module_base .. path_module)
         module.init()
     end
 end
