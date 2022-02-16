9ea68f95e8a9f1ce1fe167d1aea687a885e00fe8
Kesse Jones <kjonesfc@outlook.com>
2022-02-16 15:46:05 -0300

    (refactor) moved indent-blankline config to setup function

--- lua/kvim/core/indent-blankline.lua
+++ lua/kvim/core/indent-blankline.lua
@@ -1,12 +1,11 @@
 local M = {}
 
 function M.init()
-    vim.g.indent_blankline_filetype_exclude = { "help", "startfy", "dashboard", "packer" }
-    vim.g.indent_blankline_bufname_exclude = { "terminal", "startfy" }
-
     require("indent_blankline").setup({
         show_current_context = true,
         show_current_context_start = true,
+        bufname_exclude = { "terminal", "startfy", "NvimTree", "ToggleTerm" },
+        buftype_exclude = { "help", "startfy", "dashboard", "packer" },
     })
 end
 
