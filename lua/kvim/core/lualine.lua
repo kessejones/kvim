b86e08c11cae74b6a533b4d6858bbc37d684155a
Kesse Jones <kjonesfc@outlook.com>
2022-02-16 15:44:55 -0300

    (feat) change sections in lualine

--- lua/kvim/core/lualine.lua
+++ lua/kvim/core/lualine.lua
@@ -7,27 +7,31 @@ function M.init()
             theme = "auto",
             component_separators = { left = "", right = "" },
             section_separators = { left = "", right = "" },
-            disabled_filetypes = { "NvimTree" },
+            disabled_filetypes = {},
             always_divide_middle = true,
         },
         sections = {
             lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = { "filename" },
-            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_b = { "branch" },
+            lualine_c = { { "filename", file_status = false, path = 0 } },
+            lualine_x = { { "diagnostics", source = { "nvim_lsp" } }, "encoding", "filetype" },
             lualine_y = { "progress" },
             lualine_z = { "location" },
         },
         inactive_sections = {
             lualine_a = {},
             lualine_b = {},
-            lualine_c = { "filename" },
+            lualine_c = { { "filename", file_status = true, path = 1 } },
             lualine_x = { "location" },
             lualine_y = {},
             lualine_z = {},
         },
         tabline = {},
-        extensions = {},
+        extensions = {
+            "nvim-tree",
+            "fugitive",
+            "toggleterm",
+        },
     })
 end
 
