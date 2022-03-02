4a0aff285abe5fa99bbb5c73a1440457a99ba8d5
Kesse Jones <kjonesfc@outlook.com>
2022-03-02 08:50:34 -0300

    (feat) added hack for toggle floaterm on macos

--- lua/kvim/core/floaterm.lua
+++ lua/kvim/core/floaterm.lua
@@ -3,11 +3,15 @@ local M = {}
 
 function M:init()
     keymappings.load({
+        normal_mode = {
+            ["[bs"] = '<cmd>:FloatermToggle<CR>', -- NOTE: [bs is equal to ctrl+\ (hack for macos)
+        },
         terminal_mode = {
             ["<C-n>"] = "<C-\\><C-n>:FloatermNew<CR>",
             ["<C-k>"] = "<C-\\><C-n>:FloatermKill<CR>",
             ["<C-l>"] = "<C-\\><C-n>:FloatermNext<CR>",
             ["<C-h>"] = "<C-\\><C-n>:FloatermPrev<CR>",
+            ["[bs"] = "<C-\\><C-n>:FloatermHide<CR>",
         },
     })
 
