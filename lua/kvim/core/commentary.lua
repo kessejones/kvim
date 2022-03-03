f8c4d43b5e22204b4e52487a94d6248d8360376c
Kesse Jones <kjonesfc@outlook.com>
2022-03-03 09:15:51 -0300

    (refactor) added hack for ctrl-/ comentary macos

--- lua/kvim/core/commentary.lua
+++ lua/kvim/core/commentary.lua
@@ -2,14 +2,17 @@ local keymappings = require("kvim.keymappings")
 local M = {}
 
 function M.init()
+    -- NOTE: \x2f is equal to ctrl+/ (hack for macos)
+    local ctrl_slash = "\x2f"
+
     local default_mappings = {
         normal_mode = {
             ["<C-_>"] = ":Commentary<CR>",
-            ["<Space>;"] = ":Commentary<CR>",
+            [ctrl_slash] = ":Commentary<CR>",
         },
         visual_mode = {
             ["<C-_>"] = ":Commentary<CR>",
-            ["<Space>;"] = ":Commentary<CR>",
+            [ctrl_slash] = ":Commentary<CR>",
         },
     }
 
