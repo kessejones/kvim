0f8f4862a96a390f2eb6c2f0d6dd28756ca7cee9
Kesse Jones <kjonesfc@outlook.com>
2022-03-08 11:41:55 -0300

    (fix) fixed keymapping for floaterm

--- lua/kvim/core/floaterm.lua
+++ lua/kvim/core/floaterm.lua
@@ -2,26 +2,25 @@ local keymappings = require("kvim.keymappings")
 local M = {}
 
 function M.init()
-    -- NOTE: \x1f is equal to ctrl+\ (hack for macos)
-    local ctrl_bslash = "\x1f"
+    local keymap_toggle = "<C-\\>"
+    if vim.fn.has('mac') then
+        -- NOTE: ^\ is equal to ctrl+\ (hack for macos)
+        keymap_toggle = "^\\"
+    end
 
     keymappings.load({
-        normal_mode = {
-            [ctrl_bslash] = '<cmd>:FloatermToggle<CR>',
-        },
         terminal_mode = {
-            ["<C-i>"] = "<C-\\><C-n>:FloatermNew<CR>",
-            ["<C-k>"] = "<C-\\><C-n>:FloatermKill<CR>",
+            ["<C-o>"] = "<C-\\><C-n>:FloatermNew<CR>",
             ["<C-p>"] = "<C-\\><C-n>:FloatermPrev<CR>",
             ["<C-n>"] = "<C-\\><C-n>:FloatermNext<CR>",
-            [ctrl_bslash] = "<C-\\><C-n>:FloatermHide<CR>",
+            ["<C-e>"] = "<C-\\><C-n>",
         },
     })
 
     vim.g.floaterm_title = "Terminal ($1/$2)"
     vim.g.floaterm_width = 0.99
     vim.g.floaterm_height = 0.99
-    vim.g.floaterm_keymap_toggle = "<C-\\>"
+    vim.g.floaterm_keymap_toggle = keymap_toggle
 end
 
 return M
