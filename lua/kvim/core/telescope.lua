f9cca40bd3893f8810468b996314594593632796
Kesse Jones <kjonesfc@outlook.com>
2022-04-06 19:11:06 -0300

    (feat) set default find_command to rg

--- lua/kvim/core/telescope.lua
+++ lua/kvim/core/telescope.lua
@@ -41,6 +41,7 @@ function M.init()
             find_files = {
                 hidden = true,
                 no_ignore = true,
+                find_command = { 'rg', '--files' }
             },
         },
     })
