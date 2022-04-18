4096cac8dc18c74df0d4a1564a4fa1aa9f04f231
Kesse Jones <kjonesfc@outlook.com>
2022-04-18 14:23:27 -0300

    (fix) fixed indent-blankline on nvim-tree

--- lua/kvim/core/indent-blankline.lua
+++ lua/kvim/core/indent-blankline.lua
@@ -6,8 +6,9 @@ function M.init()
     indent_blankline.setup({
         show_current_context = true,
         show_current_context_start = true,
-        bufname_exclude = { "terminal", "startfy", "NvimTree", "ToggleTerm" },
-        buftype_exclude = { "help", "startfy", "dashboard", "packer" },
+        bufname_exclude = { "terminal", "startfy" },
+        buftype_exclude = { "help", "startfy", "dashboard", "packer", "fugitive" },
+        filetype_exclude = { "NvimTree", "floaterm" }
     })
 end
 
