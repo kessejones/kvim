6757fb8dffee7302b0fd9c195fe7870a78b3f1ba
Kesse Jones <kjonesfc@outlook.com>
2022-06-24 08:49:45 -0300

    chore: neogit disable hint on top of the panel

--- lua/kvim/core/git-fugitive.lua
+++ lua/kvim/core/git-fugitive.lua
@@ -6,6 +6,7 @@ local M = {}
 function M.init()
     neogit.setup({
         disable_commit_confirmation = true,
+        disable_hint = true,
         integrations = {
             diffview = true,
         },
