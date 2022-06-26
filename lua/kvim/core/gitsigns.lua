28ee4bb9b23aa44337fa480e0e3022e2e328b013
Kesse Jones <kjonesfc@outlook.com>
2022-06-25 22:10:48 -0300

    chore: added keymappings to gitsigns

--- lua/kvim/core/gitsigns.lua
+++ lua/kvim/core/gitsigns.lua
@@ -6,12 +6,26 @@ local M = {}
 function M.init()
     gitsigns.setup({
         current_line_blame = false,
-    })
+        on_attach = function(bufnr)
+            local mappigns = {
+                normal_mode = {
+                    ["<Leader>hs"] = gitsigns.stage_hunk,
+                    ["<Leader>hu"] = gitsigns.undo_stage_hunk,
+                    ["<Leader>hd"] = gitsigns.diffthis,
+                    ["<Leader>hR"] = gitsigns.reset_buffer,
+                    ["<Leader>hb"] = function()
+                        gitsigns.blame_line({ full = true })
+                    end,
+                    ["<Leader>td"] = gitsigns.toggle_deleted,
+                },
+                visual_mode = {
+                    ["<Leader>hs"] = gitsigns.stage_hunk,
+                    ["<Leader>hr"] = gitsigns.reset_hunk,
+                },
+            }
 
-    keymappings.load({
-        normal_mode = {
-            ["<Leader>td"] = ":Gitsigns toggle_deleted<CR>",
-        },
+            keymappings.load(mappigns, bufnr)
+        end,
     })
 end
 
