d377dae4359f5ca3986c39e16189a659fe8ba081
Kesse Jones <kjonesfc@outlook.com>
2022-09-29 14:44:36 -0300

    chore: refactor color module

--- lua/kvim/colors/init.lua
+++ lua/kvim/colors/init.lua
@@ -4,25 +4,23 @@ local settings = require("kvim.config.settings")
 local M = {}
 
 function M.init()
-    if not vim.fn.has("macunix") and vim.fn.has("termguicolors") then
+    if not utils.is_macos() and utils.has_termguicolors() then
         settings.set("termguicolors", true)
     end
 
-    if utils.contains(vim.fn.getcompletion("", "color"), "kanagawa") then
-        require("kanagawa").setup({
-            undercurl = true,
-            commentStyle = { italic = true },
-            variablebuiltinStyle = { italic = true },
-            specialReturn = true,
-            specialException = true,
-            transparent = false,
-            globalStatus = true,
-            colors = {},
-            overrides = {},
-        })
+    require("kanagawa").setup({
+        undercurl = true,
+        commentStyle = { italic = true },
+        variablebuiltinStyle = { italic = true },
+        specialReturn = true,
+        specialException = true,
+        transparent = false,
+        globalStatus = true,
+        colors = {},
+        overrides = {},
+    })
 
-        settings.cmd("colorscheme", "kanagawa")
-    end
+    settings.cmd("colorscheme", "kanagawa")
 
     vim.api.nvim_set_hl(0, "NormalFloat", {
         background = "#1F1F28",
