199860a18069a39342b516d416001391beb387f8
Kesse Jones <kjonesfc@outlook.com>
2022-10-04 22:42:10 -0300

    chore: added phpactor lsp

--- lua/kvim/core/lsp.lua
+++ lua/kvim/core/lsp.lua
@@ -65,7 +65,13 @@ local servers = {
     yamlls = {},
     html = {},
     cssls = {},
-    intelephense = {},
+    -- intelephense = {},
+    phpactor = {
+        init_options = {
+            ["language_server_phpstan.enabled"] = false,
+            ["language_server_psalm.enabled"] = false,
+        },
+    },
     elixirls = {
         cmd = { vim.fn.expand("$HOME") .. "/.local/bin/elixir-ls" },
     },
