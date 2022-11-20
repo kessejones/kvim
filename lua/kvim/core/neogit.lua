3c510beff06653292d9d29eb3919d4d2aea929c5
Kesse Jones <kjonesfc@outlook.com>
2022-11-20 10:42:15 -0300

    feat(diffview): added key mapping to open diff and open history

--- lua/kvim/core/neogit.lua
+++ lua/kvim/core/neogit.lua
@@ -1,6 +1,9 @@
 local neogit = require("neogit")
 local keymappings = require("kvim.keymappings")
 
+local au = vim.api.nvim_create_autocmd
+local ag = vim.api.nvim_create_augroup
+
 local M = {}
 
 function M.init()
@@ -17,7 +20,39 @@ function M.init()
             ["<Leader>gg"] = function()
                 neogit.open({})
             end,
+            ["<Leader>gv"] = function()
+                if vim.g.kvim_diffview_opened then
+                    require("diffview").close()
+                else
+                    require("diffview").open()
+                end
+            end,
+            ["<Leader>gf"] = function()
+                if vim.g.kvim_diffview_opened then
+                    require("diffview").close()
+                else
+                    require("diffview").file_history()
+                end
+            end,
         },
+    })
+
+    ag("Kvim", {})
+
+    au("User", {
+        group = "Kvim",
+        pattern = "DiffviewViewOpened",
+        callback = function()
+            vim.g.kvim_diffview_opened = true
+        end,
+    })
+
+    au("User", {
+        group = "Kvim",
+        pattern = "DiffviewViewClosed",
+        callback = function()
+            vim.g.kvim_diffview_opened = false
+        end,
     })
 end
 
