6e37b1c7f76281ac439f8c51f9a1b7f12103e4cf
Kesse Jones <kjonesfc@outlook.com>
2023-01-17 20:02:34 -0300

    chore: setup lazy load to nvimtree

--- lua/kvim/plugins/nvimtree.lua
+++ lua/kvim/plugins/nvimtree.lua
@@ -1,6 +1,14 @@
 return {
     "kyazdani42/nvim-tree.lua",
     dependencies = { "kyazdani42/nvim-web-devicons" },
+    keys = {
+        {
+            "<C-g>",
+            function()
+                require("nvim-tree").toggle()
+            end,
+        },
+    },
     config = function()
         require("kvim.core.nvimtree").init()
     end,
