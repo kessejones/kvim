cc71933177c181c4421b741b8c3b260a0f50baea
Kesse Jones <kjonesfc@outlook.com>
2023-01-17 20:02:53 -0300

    chore: setup dap to php and golang

--- lua/kvim/core/dap/init.lua
+++ lua/kvim/core/dap/init.lua
@@ -8,9 +8,43 @@ local dap_virtual_text = require("nvim-dap-virtual-text")
 local function setup_adapters()
     dap.adapters.php = {
         type = "executable",
-        command = "node",
-        args = { "/home/kesse/.temp/vscode-php-debug/out/phpDebug.js" },
+        command = "php-debug-adapter",
     }
+
+    dap.adapters.go = function(callback, _)
+        local stdout = vim.loop.new_pipe(false)
+        local handle, pid_or_err
+        local port = 38697
+
+        handle, pid_or_err = vim.loop.spawn("dlv", {
+            stdio = { nil, stdout },
+            args = { "dap", "-l", "127.0.0.1:" .. port },
+            detached = true,
+        }, function(code)
+            stdout:close()
+            handle:close()
+
+            print("[delve] Exit Code:", code)
+        end)
+
+        assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+
+        stdout:read_start(function(err, chunk)
+            assert(not err, err)
+
+            if chunk then
+                vim.schedule(function()
+                    require("dap.repl").append(chunk)
+                    print("[delve]", chunk)
+                end)
+            end
+        end)
+
+        -- Wait for delve to start
+        vim.defer_fn(function()
+            callback({ type = "server", host = "127.0.0.1", port = port })
+        end, 100)
+    end
 
     dap.configurations.php = {
         {
@@ -19,6 +53,41 @@ local function setup_adapters()
             name = "Listen for xdebug",
             port = "9000",
             log = true,
+        },
+    }
+
+    dap.configurations.go = {
+        {
+            type = "go",
+            name = "Debug (from vscode-go)",
+            request = "launch",
+            showLog = false,
+            program = "${file}",
+            dlvToolPath = vim.fn.exepath("dlv"), -- Adjust to where delve is installed
+        },
+        {
+            type = "go",
+            name = "Debug (No File)",
+            request = "launch",
+            program = "",
+        },
+        {
+            type = "go",
+            name = "Debug",
+            request = "launch",
+            program = "${file}",
+            showLog = true,
+            -- console = "externalTerminal",
+            -- dlvToolPath = vim.fn.exepath "dlv",
+        },
+        {
+            name = "Test Current File",
+            type = "go",
+            request = "launch",
+            showLog = true,
+            mode = "test",
+            program = ".",
+            dlvToolPath = vim.fn.exepath("dlv"),
         },
     }
 end
