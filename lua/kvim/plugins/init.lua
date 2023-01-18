485afee2e7d3d5a4941f920901f41acd2f5fb05f
Kesse Jones <kjonesfc@outlook.com>
2023-01-18 20:42:01 -0300

    chore: change spectre keymappings

--- lua/kvim/plugins/init.lua
+++ lua/kvim/plugins/init.lua
@@ -21,8 +21,20 @@ return {
         "nvim-pack/nvim-spectre",
         lazy = true,
         keys = {
-            { ",s", "<cmd>lua require('spectre').open()<cr>", desc = "Spectre" },
-            { ",f", "<cmd>lua require('spectre').open_file_search()<cr>", desc = "Spectre file search" },
+            {
+                ";s",
+                function()
+                    require("spectre").open()
+                end,
+                desc = "Spectre find all files",
+            },
+            {
+                ";f",
+                function()
+                    require("spectre").open_file_search()
+                end,
+                desc = "Spectre file search",
+            },
         },
     },
 
