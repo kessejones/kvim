a357cc7df1a1e6c517d66d3478d6ab191abf87e8
Kesse Jones <kjonesfc@outlook.com>
2023-02-17 23:07:44 -0300

    chore: update git-blame-line plugin config

--- lua/kvim/core/git-blame-line.lua
+++ lua/kvim/core/git-blame-line.lua
@@ -1,22 +1,9 @@
-local keymappings = require("kvim.keymappings")
 local git_blame_line = require("git-blame-line")
 
 local M = {}
 
 function M.init()
-    git_blame_line.setup({
-        view = {
-            enable_cursor_hold = true,
-        },
-    })
-
-    local mappings = {
-        normal_mode = {
-            ["<S-T>"] = "<cmd>:GitBlameLineToggle<CR>",
-        },
-    }
-
-    keymappings.load(mappings)
+    git_blame_line.setup()
 end
 
 return M
--- lua/kvim/plugins/git-blame-line.lua
+++ lua/kvim/plugins/git-blame-line.lua
@@ -1,7 +1,24 @@
 return {
     "kessejones/git-blame-line.nvim",
-    event = { "InsertEnter" },
+    branch = "diffview-integration",
     cmd = "GitBlameLineToggle",
+    keys = {
+        {
+            "<S-t>",
+            function()
+                require("git-blame-line").toggle()
+            end,
+        },
+        {
+            "<Leader>gt",
+            function()
+                require("git-blame-line").open_diffview()
+            end,
+        },
+    },
+    dependencies = {
+        "sindrets/diffview.nvim",
+    },
     config = function()
         require("kvim.core.git-blame-line").init()
     end,
