b3eb34215c6c91c4c9f0b75216da034382c5b334
Kesse Jones <kjonesfc@outlook.com>
2023-02-21 14:03:30 -0300

    refactor: remove unnecessary import of catppuccin colors

--- lua/kvim/colors/init.lua
+++ lua/kvim/colors/init.lua
@@ -1,27 +1,19 @@
 local M = {}
 
 function M.init()
-    local flavour = "mocha"
-    local colors = require("catppuccin.palettes").get_palette(flavour)
-
     require("catppuccin").setup({
-        flavour = flavour,
-        styles = {
-            -- comments = { "italic" },
-            -- conditionals = { "italic" },
-            -- loops = { "italic" },
-            -- types = { "italic" },
-            -- keywords = { "italic" },
-        },
-        custom_highlights = {
-            FloatBorder = { fg = colors.blue, bg = colors.base },
-            TermBorder = { link = "FloatBorder" },
-            NormalFloat = { fg = colors.text, bg = colors.base },
-            TelescopeBorder = { link = "FloatBorder" },
-            IndentBlanklineContextChar = { fg = colors.surface2 },
-            WinSeparator = { fg = colors.blue },
-            Breakpoint = { fg = colors.red },
-        },
+        flavour = "mocha",
+        custom_highlights = function(colors)
+            return {
+                FloatBorder = { fg = colors.blue, bg = colors.base },
+                TermBorder = { link = "FloatBorder" },
+                NormalFloat = { fg = colors.text, bg = colors.base },
+                TelescopeBorder = { link = "FloatBorder" },
+                IndentBlanklineContextChar = { fg = colors.surface2 },
+                WinSeparator = { fg = colors.blue },
+                Breakpoint = { fg = colors.red },
+            }
+        end,
     })
 
     vim.cmd.colorscheme("catppuccin")
