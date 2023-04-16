6f362c392aa79b7c71e3406d3ae959e9ca4a9970
Kesse Jones <kjonesfc@outlook.com>
2023-04-16 14:59:21 -0300

    chore: remove comments

--- lua/kvim/bootstrap.lua
+++ lua/kvim/bootstrap.lua
@@ -1,7 +1,5 @@
--- local plugins = require("kvim.plugins")
 local colors = require("kvim.colors")
 local plugin_manager = require("kvim.plugin-manager")
--- local core = require("kvim.core")
 local keymappings = require("kvim.keymappings")
 local config = require("kvim.config")
 local ui = require("kvim.ui")
@@ -13,10 +11,7 @@ function M.init()
     keymappings.init()
 
     plugin_manager.init()
-    -- plugin_manager.load(plugins)
 
-    colors.init()
-    -- core.init()
     ui.init()
 end
 
