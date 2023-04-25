df5589a2855a0413a76ff79a261fcd5e4e14cb0b
Kesse Jones <kjonesfc@outlook.com>
2023-04-24 21:45:14 -0300

    refactor: command TermNew replaced by `Term` with params `vertical`, `horizontal` and `tab`

--- after/plugin/term.lua
+++ after/plugin/term.lua
@@ -16,7 +16,30 @@ a.nvim_create_autocmd("TermOpen", {
     end,
 })
 
-a.nvim_create_user_command("TermNew", function()
-    vim.cmd.vsplit()
+local function term_command_run(opts)
+    local kind = opts.fargs[1]
+    if kind == "horizontal" or not kind then
+        vim.cmd.split({ mods = { split = "botright" } })
+    elseif kind == "vertical" then
+        vim.cmd.split({ mods = { vertical = true, split = "topleft" } })
+    elseif kind == "tab" then
+        vim.cmd.tabnew()
+    end
     vim.cmd.term()
-end, {})
+end
+
+local function term_command_complete(_, line, _)
+    local input = vim.fn.trim(line)
+
+    if #(vim.split(input, " ")) > 1 then
+        return {}
+    end
+
+    return { "vertical", "horizontal", "tab" }
+end
+
+a.nvim_create_user_command("Term", term_command_run, {
+    nargs = "*",
+    range = true,
+    complete = term_command_complete,
+})
