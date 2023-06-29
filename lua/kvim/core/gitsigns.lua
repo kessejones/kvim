0a488b490cda133017a280d3714989216740e707
Kesse Jones <kjonesfc@outlook.com>
2023-06-29 14:32:45 -0300

    chore: change keymappings to gitsigns

--- lua/kvim/core/gitsigns.lua
+++ lua/kvim/core/gitsigns.lua
@@ -6,21 +6,28 @@ local M = {}
 function M.init()
     gitsigns.setup({
         current_line_blame = false,
+        numhl = true,
         on_attach = function(bufnr)
             local mappigns = {
                 normal_mode = {
-                    ["<Leader>,s"] = gitsigns.stage_hunk,
-                    ["<Leader>,u"] = gitsigns.undo_stage_hunk,
-                    ["<Leader>,r"] = gitsigns.reset_buffer,
-                    ["<Leader>,d"] = gitsigns.diffthis,
-                    ["<Leader>,b"] = function()
+                    ["<leader>hs"] = gitsigns.stage_hunk,
+                    ["<leader>hr"] = gitsigns.reset_hun,
+                    ["<leader>hS"] = gitsigns.stage_buffer,
+                    ["<leader>hu"] = gitsigns.undo_stage_hunk,
+                    ["<leader>hR"] = gitsigns.reset_buffer,
+                    ["<leader>hp"] = gitsigns.preview_hunk,
+                    ["<leader>hb"] = function()
                         gitsigns.blame_line({ full = true })
                     end,
-                    ["<Leader>,t"] = gitsigns.toggle_deleted,
+                    ["<leader>hg"] = gitsigns.toggle_deleted,
                 },
                 visual_mode = {
-                    ["<Leader>,s"] = gitsigns.stage_hunk,
-                    ["<Leader>,r"] = gitsigns.reset_hunk,
+                    ["<leader>hs"] = function()
+                        gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end,
+                    ["<leader>hr"] = function()
+                        gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end,
                 },
             }
 
