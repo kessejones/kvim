9efccf3034ce9a484e8d7453e49442e17ebac101
Kesse Jones <kjonesfc@outlook.com>
2023-11-03 23:27:22 -0300

    feat: set asm comment char to `#` instead `;`

--- lua/kvim/core/comment.lua
+++ lua/kvim/core/comment.lua
@@ -15,6 +15,7 @@ function M.init()
     local comment_ft = require("Comment.ft")
     comment_ft.set("http", { "# %s", "# %s" })
     comment_ft.set("mysql", { "-- %s", "-- %s" })
+    comment_ft.set("asm", { "# %s", "# %s" })
 
     keymappings.load({
         visual_mode = {
