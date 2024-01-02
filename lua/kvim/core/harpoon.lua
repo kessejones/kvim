ca296193753c8c589c147f404fd4c7e00d6ede15
Kesse Jones <kjonesfc@outlook.com>
2024-01-01 21:34:11 -0300

    feat: using harpoon2

--- lua/kvim/core/harpoon.lua
+++ lua/kvim/core/harpoon.lua
@@ -1,14 +1,63 @@
+local harpoon = require("harpoon")
+
 local M = {}
 
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+    local file_paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+    end
+
+    require("telescope.pickers")
+        .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+                results = file_paths,
+            }),
+            previewer = conf.file_previewer({}),
+            sorter = conf.generic_sorter({}),
+        })
+        :find()
+end
+
 function M.init()
-    require("harpoon").setup()
+    harpoon.setup({
+        settings = {
+            save_on_toggle = true,
+        },
+    })
 
     require("kvim.keymappings").load({
         normal_mode = {
-            ["tl"] = require("harpoon.ui").nav_next,
-            ["th"] = require("harpoon.ui").nav_prev,
-            ["ta"] = require("harpoon.mark").add_file,
-            ["tt"] = require("harpoon.ui").toggle_quick_menu,
+            ["tl"] = function()
+                harpoon:list():next()
+            end,
+            ["th"] = function()
+                harpoon:list():prev()
+            end,
+            ["ta"] = function()
+                harpoon:list():append()
+            end,
+            ["tt"] = function()
+                harpoon.ui:toggle_quick_menu(harpoon:list(), {
+                    title_pos = "center",
+                    ui_width_ratio = 0.3,
+                    border = {
+                        "╭",
+                        "─",
+                        "╮",
+                        "│",
+                        "╯",
+                        "─",
+                        "╰",
+                        "│",
+                    },
+                })
+            end,
+            ["tp"] = function()
+                toggle_telescope(harpoon:list())
+            end,
         },
     })
 end
--- lua/kvim/plugins/harpoon.lua
+++ lua/kvim/plugins/harpoon.lua
@@ -1,10 +1,12 @@
 return {
     "theprimeagen/harpoon",
+    branch = "harpoon2",
     keys = {
         "tt",
         "tl",
         "th",
         "ta",
+        "tp",
     },
     config = function()
         require("kvim.core.harpoon").init()
