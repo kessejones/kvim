cabb1b61f442559b73e2c4c4981bee989b1b3cc5
Kesse Jones <kjonesfc@outlook.com>
2024-03-28 23:25:00 -0300

    feat: added new database plugin

--- lua/kvim/plugins/database.lua
+++ lua/kvim/plugins/database.lua
@@ -1,15 +1,49 @@
+-- return {
+--     "kristijanhusak/vim-dadbod-ui",
+--     cmd = "DBUIToggle",
+--     keys = {
+--         {
+--             "<Leader>u",
+--             ":DBUIToggle<CR>",
+--             desc = "Toggle Dadbod UI",
+--         },
+--     },
+--     dependencies = {
+--         "tpope/vim-dadbod",
+--         "kristijanhusak/vim-dadbod-completion",
+--     },
+-- }
+
 return {
-    "kristijanhusak/vim-dadbod-ui",
-    cmd = "DBUIToggle",
-    keys = {
-        {
-            "<Leader>u",
-            ":DBUIToggle<CR>",
-            desc = "Toggle Dadbod UI",
-        },
-    },
+    "kndndrj/nvim-dbee",
     dependencies = {
-        "tpope/vim-dadbod",
-        "kristijanhusak/vim-dadbod-completion",
+        "MunifTanjim/nui.nvim",
     },
+    build = function()
+        require("dbee").install()
+    end,
+    config = function()
+        require("dbee").setup({
+            drawer = {
+                disable_help = true,
+            },
+            editor = {
+                mappings = {
+                    { key = "<Leader>n", mode = "v", action = "run_selection" },
+                    { key = "<Leader>n", mode = "n", action = "run_file" },
+                },
+            },
+        })
+
+        require("kvim.keymappings").load({
+            normal_mode = {
+                ["<leader>u"] = function()
+                    require("dbee").toggle()
+                end,
+                ["<leader>"] = function()
+                    require("dbee").open()
+                end,
+            },
+        })
+    end,
 }
