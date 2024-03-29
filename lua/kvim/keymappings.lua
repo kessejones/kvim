1c228d5bdb820f67b3ca00b1050768a4c74d4992
Kesse Jones <kjonesfc@outlook.com>
2024-03-29 20:57:26 -0300

    feat: added custom indent key bind

--- lua/kvim/keymappings.lua
+++ lua/kvim/keymappings.lua
@@ -9,6 +9,55 @@ local mode_adapter = {
     terminal_mode = "t",
 }
 
+local function indent(dir, visual)
+    visual = visual or false
+    local modes = setmetatable({
+        visual = {
+            left = "<gv",
+            right = ">gv",
+        },
+        normal = {
+            left = "<<",
+            right = ">>",
+        },
+    }, {
+        __call = function(tbl, d, v)
+            local mode = v and "visual" or "normal"
+            return tbl[mode][d]
+        end,
+    })
+
+    local cursor = setmetatable({
+        left = function()
+            local cur = vim.api.nvim_win_get_cursor(0)
+            local col = cur[2] - vim.bo.tabstop
+            if col < 0 then
+                return cur
+            end
+
+            return { cur[1], col }
+        end,
+        right = function()
+            local cur = vim.api.nvim_win_get_cursor(0)
+            local col = cur[2] + vim.bo.tabstop
+            return { cur[1], col }
+        end,
+    }, {
+        __call = function(tbl, d)
+            return tbl[d]()
+        end,
+        __index = function()
+            assert(false, "invalid function")
+        end,
+    })
+
+    local mode = modes(dir, visual)
+    local cur = cursor(dir)
+
+    vim.cmd.normal({ bang = true, args = { mode } })
+    vim.api.nvim_win_set_cursor(0, cur)
+end
+
 local function move_buffer_to_tab(dir)
     vim.validate({
         dir = {
@@ -153,8 +202,12 @@ local default_mappings = {
         -- Select all and yank
         ["<Leader>ay"] = "ggVGy",
         -- Indent
-        ["<"] = "<<<ESC>",
-        [">"] = ">><ESC>",
+        ["<"] = function()
+            indent("left")
+        end,
+        [">"] = function()
+            indent("right")
+        end,
         ["gl"] = "$",
         ["gh"] = "^",
 
@@ -166,8 +219,15 @@ local default_mappings = {
         ["<C-h>"] = ":s/",
         ["p"] = '"_dP',
         -- Indent
-        ["<"] = "<gv",
-        [">"] = ">gv",
+        -- ["<"] = "<gv",
+        -- [">"] = ">gv",
+        ["<"] = function()
+            indent("left", true)
+        end,
+        [">"] = function()
+            indent("right", true)
+        end,
+        -- [">"] = ">gv",
         -- Move up/down selected text
         ["<C-j>"] = ":m '>+1<CR>gv=gv",
         ["<C-k>"] = ":m '<-2<CR>gv=gv",
