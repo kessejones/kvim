2e2bbd9375b895a83ef8201a2040163fb3d95b6c
Kesse Jones <kjonesfc@outlook.com>
2024-09-21 21:46:27 -0300

    chore: added nvim various textobjs for testing

--- lua/kvim/plugins/nvim-treesitter.lua
+++ lua/kvim/plugins/nvim-treesitter.lua
@@ -5,6 +5,18 @@ return {
     cmd = { "TSUpdate", "TSUpdateSync", "TSInstall" },
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
+        {
+            "chrisgrieser/nvim-various-textobjs",
+            config = function()
+                require("various-textobjs").setup({
+                    lookForwardSmall = 5,
+                    lookForwardBig = 15,
+                    useDefaultKeymaps = true,
+                    disabledKeymaps = {},
+                    notifyNotFound = true,
+                })
+            end,
+        },
     },
     config = function()
         require("kvim.core.treesitter").init()
