b8f4e350149b315c31f6475a1ec44bd6e764be26
Kesse Jones <kjonesfc@outlook.com>
2025-01-21 22:50:10 -0300

    chore: disable nushell plugin

--- lua/kvim/plugins/nushell.lua
+++ lua/kvim/plugins/nushell.lua
@@ -1,8 +1,10 @@
 return {
     "LhKipp/nvim-nu",
+    enabled = false,
     ft = {
         "nu",
     },
+    build = ":TSInstall nu",
     config = function()
         require("nu").setup({})
     end,
