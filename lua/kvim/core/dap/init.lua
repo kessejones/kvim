3f45118a6e054d8fa456fbb1fa6145351bc2b85a
Kesse Jones <kjonesfc@outlook.com>
2025-04-08 00:13:35 -0300

    feat: setup dap debug for zig

--- lua/kvim/core/dap/init.lua
+++ lua/kvim/core/dap/init.lua
@@ -1,85 +1,28 @@
 local M = {}
 
 local keymappings = require("kvim.utils.keymap")
+
 local dap = require("dap")
 local dapui = require("dapui")
 local dap_virtual_text = require("nvim-dap-virtual-text")
 
 local function setup_adapters()
-    dap.adapters.php = {
+    dap.adapters.gdb = {
+        id = "gdb",
         type = "executable",
-        command = "php-debug-adapter",
+        command = "gdb",
+        args = { "--quiet", "--interpreter=dap" },
+        target = "localhost:1234",
     }
 
-    dap.adapters.go = function(callback, _)
-        local stdout = vim.loop.new_pipe(false)
-        local handle, pid_or_err
-        local port = 38697
+    dap.configurations.go = {}
 
-        handle, pid_or_err = vim.loop.spawn("dlv", {
-            stdio = { nil, stdout },
-            args = { "dap", "-l", "127.0.0.1:" .. port },
-            detached = true,
-        }, function(code)
-            stdout:close()
-            handle:close()
-
-            print("[delve] Exit Code:", code)
-        end)
-
-        assert(handle, "Error running dlv: " .. tostring(pid_or_err))
-
-        stdout:read_start(function(err, chunk)
-            assert(not err, err)
-
-            if chunk then
-                vim.schedule(function()
-                    require("dap.repl").append(chunk)
-                    print("[delve]", chunk)
-                end)
-            end
-        end)
-
-        -- Wait for delve to start
-        vim.defer_fn(function()
-            callback({ type = "server", host = "127.0.0.1", port = port })
-        end, 100)
-    end
-
-    require("dap.ext.vscode").load_launchjs(nil, { xdebug = { "php" } })
-
-    dap.configurations.go = {
+    dap.configurations.zig = {
         {
-            type = "go",
-            name = "Debug (from vscode-go)",
+            name = "Attach to process (GDB)",
+            type = "gdb",
             request = "launch",
-            showLog = false,
-            program = "${file}",
-            dlvToolPath = vim.fn.exepath("dlv"), -- Adjust to where delve is installed
-        },
-        {
-            type = "go",
-            name = "Debug (No File)",
-            request = "launch",
-            program = "",
-        },
-        {
-            type = "go",
-            name = "Debug",
-            request = "launch",
-            program = "${file}",
-            showLog = true,
-            -- console = "externalTerminal",
-            -- dlvToolPath = vim.fn.exepath "dlv",
-        },
-        {
-            name = "Test Current File",
-            type = "go",
-            request = "launch",
-            showLog = true,
-            mode = "test",
-            program = ".",
-            dlvToolPath = vim.fn.exepath("dlv"),
+            cwd = "${workspaceFolder}",
         },
     }
 end
@@ -155,7 +98,7 @@ local function setup_ui()
         end,
     })
 
-    vim.fn.sign_define("DapBreakpoint", { text = "ﱣ", texthl = "Breakpoint", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "Breakpoint", linehl = "", numhl = "" })
 end
 
 function M.init()
--- lua/kvim/plugins/nvim-dap.lua
+++ lua/kvim/plugins/nvim-dap.lua
@@ -1,6 +1,10 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = { "rcarriga/nvim-dap-ui", "theHamsta/nvim-dap-virtual-text" },
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+        "rcarriga/nvim-dap",
+        "theHamsta/nvim-dap-virtual-text",
+        "nvim-neotest/nvim-nio",
+    },
     keys = {
         "<LocalLeader>da",
         "<LocalLeader>dt",
