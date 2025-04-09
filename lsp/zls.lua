af90206ec730dfdd74e3f402119ab0f2285e0fea
Kesse Jones <kjonesfc@outlook.com>
2025-04-08 23:41:15 -0300

    refact: remove unused settings on zls

--- lsp/zls.lua
+++ lsp/zls.lua
@@ -2,18 +2,5 @@
 return {
     cmd = { "zls" },
     filetypes = { "zig", "zir" },
-    root_markers = { ".git" },
-
-    settings = {
-        zls = {
-            enable_inline_hints = true,
-            enable_snippets = true,
-            warn_style = true,
-        },
-    },
-
-    on_init = function (client, init_result)
-        vim.g.zig_fmt_parse_errors = 0
-        vim.g.zig_fmt_autosave = 0
-    end
+    root_markers = { "zls.json", "build.zig", ".git" },
 }
