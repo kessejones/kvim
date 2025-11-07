2ea0096dc511853932a333217b7ca74930cb832b
Kesse Jones <kjonesfc@outlook.com>
2025-11-07 10:36:24 -0300

    feat: added utilities autocmds

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -1,5 +1,39 @@
 vim.api.nvim_create_autocmd("FileType", {
     callback = function()
-        vim.opt_local.formatoptions:remove("o")
+        vim.opt_local.formatoptions:remove({ "o", "c", "r" })
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args)
+        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.api.nvim_win_set_cursor(0, mark)
+
+            vim.schedule(function()
+                vim.cmd("normal! zz")
+            end)
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("VimResized", {
+    command = "wincmd =",
+})
+
+local group_focus_cursor = vim.api.nvim_create_augroup("focus_cursor_line", { clear = true })
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+    group = group_focus_cursor,
+    callback = function()
+        vim.opt_local.cursorline = true
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+    group = group_focus_cursor,
+    callback = function()
+        vim.opt_local.cursorline = false
     end,
 })
