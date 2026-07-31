94571966395cb048dd24030e03da298c8eb51ea9
Kesse Jones <kjonesfc@outlook.com>
2026-07-31 17:18:17 -0300

    chore: update configs

--- lua/kvim/core/blink.lua
+++ lua/kvim/core/blink.lua
@@ -5,27 +5,27 @@ blink.setup({
         -- default = { "lsp", "path", "snippets", "buffer", "copilot" },
         default = { "lsp", "path", "snippets", "buffer" },
 
-        providers = {
-            copilot = {
-                name = "copilot",
-                module = "blink-copilot",
-                opts = {
-                    max_completions = 3,
-                    max_attempts = 4,
-                },
-                score_offset = 100,
-                async = true,
-                transform_items = function(_, items)
-                    local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-                    local kind_idx = #CompletionItemKind + 1
-                    CompletionItemKind[kind_idx] = "Copilot"
-                    for _, item in ipairs(items) do
-                        item.kind = kind_idx
-                    end
-                    return items
-                end,
-            },
-        },
+        -- providers = {
+        --     copilot = {
+        --         name = "copilot",
+        --         module = "blink-copilot",
+        --         opts = {
+        --             max_completions = 3,
+        --             max_attempts = 4,
+        --         },
+        --         score_offset = 100,
+        --         async = true,
+        --         transform_items = function(_, items)
+        --             local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+        --             local kind_idx = #CompletionItemKind + 1
+        --             CompletionItemKind[kind_idx] = "Copilot"
+        --             for _, item in ipairs(items) do
+        --                 item.kind = kind_idx
+        --             end
+        --             return items
+        --         end,
+        --     },
+        -- },
     },
 
     keymap = {
